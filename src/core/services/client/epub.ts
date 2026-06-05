80d6010426291418dda4f3ad1a89764d9e76b109
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2026-06-05 16:02:10 +0700

    feat(epub): skip getAllEpub query when user is not authenticated

--- src/core/services/client/epub.ts
+++ src/core/services/client/epub.ts
@@ -1,13 +1,18 @@
 import { useQuery } from "@/core/plugins/reactQuery";
+import { useSelector } from "react-redux";
+import { RootState } from "@/core/redux/store";
 import { FetchQueryKeys } from "../endpoints";
 import { getAllEpub } from "../api/epub";
 
 export const useGetAllEpub = () => {
+  const token = useSelector((s: RootState) => s.user.token);
+
   return useQuery({
     queryKey: [FetchQueryKeys.EPUB_GET_USER_EPUB],
     queryFn: async () => {
       const response = await getAllEpub();
       return response;
     },
+    enabled: !!token,
   });
 };
